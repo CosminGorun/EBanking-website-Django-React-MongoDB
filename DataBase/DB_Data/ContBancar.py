17d185449529359ac7b40683b5d599571b64c563
CosminGorun <gorun.cosmin003@gmail.com>
2024-11-06 20:15:16 +0200

    adaugare contBancar

---
+++ DataBase/DB_Data/ContBancar.py
@@ -0,0 +1,3 @@
+# datele se salveaza in baza de date DB_User in colectia conturi
+class ContBancar:
+    __fields__ = ['userID', 'moneda',]
--- DataBase/DB_Data/User.py
+++ DataBase/DB_Data/User.py
@@ -1,6 +1,6 @@
 # datele se salveaza in baza de date DB_User in colectia Users
 class User:
-    __fields__ = ['name', 'username', 'password','mail','phoneNumber','userID','IBAN']
+    __fields__ = ['name', 'username', 'password','mail','phoneNumber','userID']
     def __init__(self, name, username, password,mail, phoneNumber):
         self.name = name
         self.username = username
